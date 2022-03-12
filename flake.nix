8f7940ac7b23e2d037fa20ec367956467b64e794
hugosenari <hugosenari@gmail.com>
2022-03-12 01:02:43 -0300

    chore: re index changer

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = ''A tool for managing a project's changelog'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."changer-master".url = "path:./master";
+  inputs."changer-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_1_1".url = "path:./v0_1_1";
+  inputs."changer-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_2_0".url = "path:./v0_2_0";
+  inputs."changer-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_3_0".url = "path:./v0_3_0";
+  inputs."changer-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_4_0".url = "path:./v0_4_0";
+  inputs."changer-v0_4_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_4_1".url = "path:./v0_4_1";
+  inputs."changer-v0_4_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_4_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_4_2".url = "path:./v0_4_2";
+  inputs."changer-v0_4_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_4_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_5_0".url = "path:./v0_5_0";
+  inputs."changer-v0_5_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_5_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_6_0".url = "path:./v0_6_0";
+  inputs."changer-v0_6_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_6_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_6_1".url = "path:./v0_6_1";
+  inputs."changer-v0_6_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_6_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_6_2".url = "path:./v0_6_2";
+  inputs."changer-v0_6_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_6_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."changer-v0_7_0".url = "path:./v0_7_0";
+  inputs."changer-v0_7_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."changer-v0_7_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_6_2/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = ''A tool for managing a project's changelog'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-changer-v0_6_2.flake = false;
+  inputs.src-changer-v0_6_2.owner = "iffy";
+  inputs.src-changer-v0_6_2.ref   = "refs/tags/v0.6.2";
+  inputs.src-changer-v0_6_2.repo  = "changer";
+  inputs.src-changer-v0_6_2.type  = "github";
+  
+  inputs."argparse".dir   = "nimpkgs/a/argparse";
+  inputs."argparse".owner = "riinr";
+  inputs."argparse".ref   = "flake-pinning";
+  inputs."argparse".repo  = "flake-nimble";
+  inputs."argparse".type  = "github";
+  inputs."argparse".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."argparse".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."regex".dir   = "nimpkgs/r/regex";
+  inputs."regex".owner = "riinr";
+  inputs."regex".ref   = "flake-pinning";
+  inputs."regex".repo  = "flake-nimble";
+  inputs."regex".type  = "github";
+  inputs."regex".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."regex".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."parsetoml".dir   = "nimpkgs/p/parsetoml";
+  inputs."parsetoml".owner = "riinr";
+  inputs."parsetoml".ref   = "flake-pinning";
+  inputs."parsetoml".repo  = "flake-nimble";
+  inputs."parsetoml".type  = "github";
+  inputs."parsetoml".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."parsetoml".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-changer-v0_6_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-changer-v0_6_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
